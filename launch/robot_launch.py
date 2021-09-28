c58f67f3427282f90203ef63a99bda770007f7cd
Manuel <115771+ManuelZ@users.noreply.github.com>
2021-09-28 15:32:28 -0500

    update launch file

--- launch/robot_launch.py
+++ launch/robot_launch.py
@@ -9,10 +9,13 @@ from webots_ros2_core.webots_launcher import WebotsLauncher
 
 
 def generate_launch_description():
-    package_dir = get_package_share_directory('slam_algorithms')
-    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
-    robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'webots_robot_description.urdf')).read_text()
+    package_dir         = get_package_share_directory('slam_algorithms')
+    use_sim_time        = LaunchConfiguration('use_sim_time', default=True)
+    world               = LaunchConfiguration('world', default='webots_world_file.wbt')
+    robot_description   = pathlib.Path(os.path.join(package_dir, 'resource', 'webots_robot_description.urdf')).read_text()
     ros2_control_params = os.path.join(package_dir, 'resource', 'ros2control.yaml')
+
+    
 
     # The WebotsLauncher is a Webots custom action that allows you to start a Webots simulation instance.
     # It searches for the Webots installation in the path specified by the `WEBOTS_HOME` environment variable and default installation paths.
@@ -21,8 +24,9 @@ def generate_launch_description():
     # - `gui` (bool): Whether to display GUI or not.
     # - `mode` (str): Can be `pause`, `realtime`, or `fast`.
     webots = WebotsLauncher(
-        world=PathJoinSubstitution([package_dir, 'worlds', 'webots_world_file.wbt'])
+        world=PathJoinSubstitution([package_dir, 'worlds', world])
     )
+
 
     # The node which interacts with a robot in the Webots simulation is located in the `webots_ros2_driver` package under name `driver`.
     # It is necessary to run such a node for each robot in the simulation.
@@ -31,6 +35,9 @@ def generate_launch_description():
         package    = 'webots_ros2_driver',
         executable = 'driver',
         output     = 'screen',
+        remappings = [
+            ('/diffdrive_controller/cmd_vel_unstamped', '/cmd_vel'),
+        ],
         parameters = [
             {
                 'robot_description': robot_description,
@@ -38,14 +45,11 @@ def generate_launch_description():
             },
             ros2_control_params
         ],
-        remappings=[
-            ('/diffdrive_controller/cmd_vel_unstamped', '/cmd_vel'),
-        ]
     )
 
     # Often we want to publish robot transforms, so we use the `robot_state_publisher` node for that.
     # If robot model is not specified in the URDF file then Webots can help us with the URDF exportation feature.
-    # Since the exportation feature is available only once the simulation has started and the `robot_state_publisher` 
+    # Since the exportation feature is available only once the simulation has started and the `robot_state_publisher`
     # node requires a `robot_description` parameter before we have to specify a dummy robot.
     robot_state_publisher = Node(
         package    = 'robot_state_publisher',
@@ -56,15 +60,18 @@ def generate_launch_description():
         }],
     )
 
+    # The base_footprint is the representation of the robot position on the floor.
+    # https://www.ros.org/reps/rep-0120.html#base-footprint
     footprint_publisher = Node(
         package    = 'tf2_ros',
         executable = 'static_transform_publisher',
         output     = 'screen',
+        # x y z qx qy qz qw frame_id child_frame_id 
         arguments  = ['0', '0', '0', '0', '0', '0', 'base_link', 'base_footprint'],
     )
 
     controller_manager_timeout = ['--controller-manager-timeout', '50'] if os.name == 'nt' else []
-    controller_manager_prefix  = 'python.exe' if os.name == 'nt' else "bash -c 'sleep 10; $0 $@' "
+    controller_manager_prefix = 'python.exe' if os.name == 'nt' else "bash -c 'sleep 10; $0 $@' "
 
     diffdrive_controller_spawner = Node(
         package    = 'controller_manager',
@@ -82,6 +89,15 @@ def generate_launch_description():
         arguments  = ['joint_state_broadcaster'] + controller_manager_timeout,
     )
 
+    occupancy_grid = Node(
+        package    = 'slam_algorithms',
+        executable = 'occupancy_grid',
+        output     = 'screen',
+        parameters = [
+            {'use_sim_time': use_sim_time},
+        ],
+    )
+
     # Standard ROS 2 launch description
     return launch.LaunchDescription([
 
@@ -89,21 +105,24 @@ def generate_launch_description():
         webots,
 
         joint_state_broadcaster_spawner,
+        
         diffdrive_controller_spawner,
 
-        # Start the Webots robot driver
         webots_robot_driver,
 
-        # Start the robot_state_publisher
         robot_state_publisher,
 
         footprint_publisher,
 
+        occupancy_grid,
+
         # This action will kill all nodes once the Webots simulation has exited
         launch.actions.RegisterEventHandler(
-            event_handler=launch.event_handlers.OnProcessExit(
-                target_action=webots,
-                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+            event_handler = launch.event_handlers.OnProcessExit(
+                target_action = webots,
+                on_exit       = [
+                    launch.actions.EmitEvent(event = launch.events.Shutdown())
+                ],
             )
         )
     ])
